c31795d311953e9b8d3a595562f72c0b66457347
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-07-25 10:49:02 +0200

    Moving integration tests to other package

--- tests/ioc_test.go
+++ tests/ioc_test.go
@@ -1,6 +1,7 @@
 package ioc
 
 import (
+	"dzaba/go-dzaba/ioc"
 	"testing"
 
 	"github.com/google/uuid"
@@ -26,17 +27,17 @@ func (impl *firstTestInterfaceImpl) GetId() uuid.UUID {
 }
 
 func Test_AddTransientSelf_WhenServiceIsRegisteredAsSelfTransientWithoutDependencies_ThenNewInstances(t *testing.T) {
-	services := NewServiceCollection()
-	err := AddTransientSelf[*firstTestInterfaceImpl](services, NewFirstTestInterface)
+	services := ioc.NewServiceCollection()
+	err := ioc.AddTransientSelf[*firstTestInterfaceImpl](services, NewFirstTestInterface)
 	assert.Nil(t, err)
 
 	provider, err := services.BuildServiceProvder()
 	assert.Nil(t, err)
 
-	service1, err := Resolve[*firstTestInterfaceImpl](provider)
+	service1, err := ioc.Resolve[*firstTestInterfaceImpl](provider)
 	assert.Nil(t, err)
 
-	service2, err := Resolve[*firstTestInterfaceImpl](provider)
+	service2, err := ioc.Resolve[*firstTestInterfaceImpl](provider)
 	assert.Nil(t, err)
 
 	assert.NotNil(t, service1)
