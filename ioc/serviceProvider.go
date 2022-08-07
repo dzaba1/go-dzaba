ef70bf0e1f589b853858ba16c28743297553f70d
Dzaba <blazej.zabialowicz@gmail.com>
2022-08-07 16:13:47 +0200

    Merging again service scope

--- ioc/serviceProvider.go
+++ ioc/serviceProvider.go
@@ -1,6 +1,8 @@
 package ioc
 
 import (
+	"dzaba/go-dzaba/collections"
+	"dzaba/go-dzaba/utils"
 	"reflect"
 
 	"github.com/google/uuid"
@@ -10,6 +12,12 @@ type Closeable interface {
 	Close() error
 }
 
+type ServiceScope interface {
+	Close() []error
+	Resolve(serviceType reflect.Type) (any, error)
+	ResolveAll(serviceType reflect.Type) ([]any, error)
+}
+
 type ServiceProvider interface {
 	ServiceScope
 
@@ -17,20 +25,85 @@ type ServiceProvider interface {
 }
 
 type serviceProviderImpl struct {
-	serviceScopeImpl
+	resolver resolver
+	services map[reflect.Type][]*registrationImpl
+	id       uuid.UUID
 }
 
 func newServiceProvider(resolver resolver,
 	services map[reflect.Type][]*registrationImpl) (ServiceProvider, error) {
 
-	p := &serviceProviderImpl{}
-	p.resolver = resolver
-	p.services = services
-	p.id = uuid.New()
+	return &serviceProviderImpl{
+		resolver: resolver,
+		services: services,
+		id:       uuid.New(),
+	}, nil
+}
 
-	return p, nil
+func (provider *serviceProviderImpl) Resolve(serviceType reflect.Type) (any, error) {
+	return provider.resolver.resolve(serviceType, provider.id)
+}
+
+func (provider *serviceProviderImpl) ResolveAll(serviceType reflect.Type) ([]any, error) {
+	result := []any{}
+
+	for _, regs := range provider.services {
+		for _, reg := range regs {
+			service, err := provider.resolver.resolveRegistration(reg, provider.id)
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, service)
+		}
+	}
+
+	return result, nil
+}
+
+func (provider *serviceProviderImpl) Close() []error {
+	errors := []error{}
+
+	for _, registrations := range provider.services {
+		for _, reg := range registrations {
+			inst := reg.lifetimeManager.Instance(provider.id)
+			if inst != nil {
+				cast, ok := inst.(Closeable)
+				if ok {
+					err := cast.Close()
+					if err != nil {
+						errors = append(errors, err)
+					}
+				}
+				reg.lifetimeManager.ClearInstance(provider.id)
+			}
+		}
+	}
+
+	return errors
 }
 
 func (provider *serviceProviderImpl) CreateScope() (ServiceScope, error) {
-	return newServiceScope(provider.resolver, provider.services)
+	return newServiceProvider(provider.resolver, provider.services)
+}
+
+func Resolve[T any](provider ServiceProvider) (T, error) {
+	service, err := provider.Resolve(utils.TypeOfGeneric[T]())
+	if err != nil {
+		return utils.DefaultGeneric[T](), err
+	}
+
+	return service.(T), nil
+}
+
+func ResolveAll[T any](provider ServiceProvider) ([]T, error) {
+	services, err := provider.ResolveAll(utils.TypeOfGeneric[T]())
+	if err != nil {
+		return nil, err
+	}
+
+	result := collections.SelectMust(services, func(element any) T {
+		return element.(T)
+	})
+
+	return result, nil
 }
